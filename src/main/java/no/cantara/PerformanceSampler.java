d9892c26bebc63eb9196dc12a4f24ce2139128c9
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2015-10-13 14:08:08 +0200

    Added initial latency and throughput performance samplers.

---
+++ src/main/java/no/cantara/PerformanceSampler.java
@@ -0,0 +1,113 @@
+package no.cantara;
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class PerformanceSampler {
+
+   private class PerformanceSamplerTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            getPrintableTimewindowLatencyStatistics().printToStandardOutput();
+            getPrintableTimewindowThroughputStatistics().printToStandardOutput();
+            resetTimeWindow();
+        }
+    }
+
+    protected final Object initializationLock = new Object();
+    protected Timer timer;
+
+    protected final Object cumulativeStatisticsLock = new Object();
+    protected final DescriptiveStatistics latencyStatistics = new DescriptiveStatistics();
+    protected long minSentTimeNanos = Long.MAX_VALUE;
+    protected final DescriptiveStatistics throughputStatistics = new DescriptiveStatistics();
+
+    protected final Object timewindowLock = new Object();
+    protected long timewindowStartNanos = -1;
+    protected DescriptiveStatistics timewindowLatencyStatistics = new DescriptiveStatistics();
+    protected long timewindowMinSentTimeNanos = Long.MAX_VALUE;
+    protected DescriptiveStatistics timewindowThroughputStatistics = new DescriptiveStatistics();
+
+    public PerformanceSampler() {
+    }
+
+    private void resetTimeWindow() {
+        synchronized (timewindowLock) {
+            minSentTimeNanos = Long.MAX_VALUE;
+            timewindowLatencyStatistics = new DescriptiveStatistics();
+            timewindowMinSentTimeNanos = Long.MAX_VALUE;
+            timewindowThroughputStatistics = new DescriptiveStatistics();
+            timewindowStartNanos = System.nanoTime();
+        }
+    }
+
+    public PrintableStatistics getPrintableLatencyStatistics() {
+        synchronized (cumulativeStatisticsLock) {
+            return new PrintableStatistics("\"Cumulative statistics for Latency", latencyStatistics.copy());
+        }
+    }
+
+    public PrintableStatistics getPrintableThroughputStatistics() {
+        synchronized (cumulativeStatisticsLock) {
+            return new PrintableStatistics("\"Cumulative statistics for Throughput", throughputStatistics.copy());
+        }
+    }
+
+    public PrintableStatistics getPrintableTimewindowLatencyStatistics() {
+        synchronized (timewindowLock) {
+            return new PrintableStatistics("\"Time-window statistics for Latency\nwindow-size: " + timewindowStartNanos, timewindowLatencyStatistics.copy());
+        }
+    }
+
+    public PrintableStatistics getPrintableTimewindowThroughputStatistics() {
+        synchronized (timewindowLock) {
+            return new PrintableStatistics("\"Time-window statistics for Throughput", timewindowThroughputStatistics.copy());
+        }
+    }
+
+    public void addMessage(long sentTimeNanos, long receivedTimeNanos) {
+        synchronized (initializationLock) {
+            if (timer == null) {
+                timer = new Timer(true);
+                timer.scheduleAtFixedRate(new PerformanceSamplerTimerTask(), 1000, 1000);
+                resetTimeWindow();
+            }
+        }
+        addLatency(sentTimeNanos, receivedTimeNanos);
+        addThroughput(sentTimeNanos, receivedTimeNanos);
+        synchronized (timewindowLock) {
+            addTimewindowLatency(receivedTimeNanos);
+            addTimewindowThroughput(sentTimeNanos, receivedTimeNanos);
+        }
+    }
+
+    private void addLatency(long sentTimeNanos, long receivedTimeNanos) {
+        long durationNanos = receivedTimeNanos - sentTimeNanos;
+        double latencyMicro = durationNanos / 1000.0;
+        latencyStatistics.addValue(latencyMicro);
+    }
+
+    private void addThroughput(long sentTimeNanos, long receivedTimeNanos) {
+        if (sentTimeNanos < minSentTimeNanos) {
+            minSentTimeNanos = sentTimeNanos;
+        }
+        double throughput = (receivedTimeNanos - minSentTimeNanos) / (double) throughputStatistics.getN();
+        throughputStatistics.addValue(throughput);
+    }
+
+    private void addTimewindowLatency(long receivedTimeNanos) {
+        long timewindowDurationNanos = receivedTimeNanos - timewindowMinSentTimeNanos;
+        double timewindowLatencyMicro = timewindowDurationNanos / 1000.0;
+        timewindowLatencyStatistics.addValue(timewindowLatencyMicro);
+    }
+
+    private void addTimewindowThroughput(long sentTimeNanos, long receivedTimeNanos) {
+        if (sentTimeNanos < timewindowMinSentTimeNanos) {
+            timewindowMinSentTimeNanos = sentTimeNanos;
+        }
+        double timewindowThroughput = (receivedTimeNanos - timewindowMinSentTimeNanos) / (double) timewindowThroughputStatistics.getN();;
+        timewindowThroughputStatistics.addValue(timewindowThroughput);
+    }
+}
---
+++ src/main/java/no/cantara/PrintableStatistics.java
@@ -0,0 +1,41 @@
+package no.cantara;
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
+import java.io.PrintStream;
+
+public class PrintableStatistics {
+    private final String description;
+    private final DescriptiveStatistics statistics;
+
+    PrintableStatistics(String description, DescriptiveStatistics statistics) {
+        this.description = description;
+        this.statistics = statistics;
+    }
+
+    public void printToStandardOutput() {
+        printTo(System.out);
+    }
+
+    public void printToStandardError() {
+        printTo(System.err);
+    }
+
+    public void printTo(PrintStream out) {
+        out.println(description);
+        out.println("N      = " + statistics.getN());
+        out.println("avg    = " + statistics.getMean());
+        out.println("stddev = " + statistics.getStandardDeviation());
+        out.println("min    = " + statistics.getMin());
+        out.println("max    = " + statistics.getMax());
+        out.println("sum    = " + statistics.getSum());
+        out.println("50%    = " + statistics.getPercentile(50));
+        out.println("95%    = " + statistics.getPercentile(90));
+        out.println("99%    = " + statistics.getPercentile(99));
+        out.println("99.9%  = " + statistics.getPercentile(99.9));
+    }
+
+    public DescriptiveStatistics getStatistics() {
+        return statistics;
+    }
+}
