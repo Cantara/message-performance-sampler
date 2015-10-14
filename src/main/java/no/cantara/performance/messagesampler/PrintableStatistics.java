9954381e707fecb46b0e8b5dce88f8d2e1f970a7
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2015-10-14 10:04:51 +0200

    Adjust width to 12.

--- src/main/java/no/cantara/performance/messagesampler/PrintableStatistics.java
+++ src/main/java/no/cantara/performance/messagesampler/PrintableStatistics.java
@@ -23,16 +23,16 @@ public class PrintableStatistics {
 
     public void printTo(PrintStream out) {
         out.println(description);
-        out.printf("N      = %6d\n", statistics.getN());
-        out.printf("avg    = %10.3f\n", statistics.getMean());
-        out.printf("stddev = %10.3f\n", statistics.getStandardDeviation());
-        out.printf("min    = %10.3f\n", statistics.getMin());
-        out.printf("max    = %10.3f\n", statistics.getMax());
-        out.printf("sum    = %10.3f\n", statistics.getSum());
-        out.printf("50%%    = %10.3f\n", statistics.getPercentile(50));
-        out.printf("95%%    = %10.3f\n", statistics.getPercentile(90));
-        out.printf("99%%    = %10.3f\n", statistics.getPercentile(99));
-        out.printf("99.9%%  = %10.3f\n", statistics.getPercentile(99.9));
+        out.printf("N      = %8d\n", statistics.getN());
+        out.printf("avg    = %12.3f\n", statistics.getMean());
+        out.printf("stddev = %12.3f\n", statistics.getStandardDeviation());
+        out.printf("min    = %12.3f\n", statistics.getMin());
+        out.printf("max    = %12.3f\n", statistics.getMax());
+        out.printf("sum    = %12.3f\n", statistics.getSum());
+        out.printf("50%%    = %12.3f\n", statistics.getPercentile(50));
+        out.printf("95%%    = %12.3f\n", statistics.getPercentile(90));
+        out.printf("99%%    = %12.3f\n", statistics.getPercentile(99));
+        out.printf("99.9%%  = %12.3f\n", statistics.getPercentile(99.9));
     }
 
     public DescriptiveStatistics getStatistics() {
